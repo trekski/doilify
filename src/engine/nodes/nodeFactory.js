9a23c4e5d6a8c84ab63b46e829ff6c8e44c4c566
Jacek Galkowski <38131406+trekski@users.noreply.github.com>
2022-01-23 14:26:52 +0100

    remove redundnat&&commented code

--- src/engine/nodes/nodeFactory.js
+++ src/engine/nodes/nodeFactory.js
@@ -1,35 +1,18 @@
 import FactoryClass from '../misc/factory.js'
+
 import CrochetNode from './crochetNode.js'
-
-// console.log(typeof CrochetNode.prototype['type'])
-
 import CrochetNodeFixed from './CrochetNodeFixed.js'
-// import CrochetNodeStart from './crochetNodeStart.js'
-// import CrochetNodeFinish from './crochetNodeFinish.js'
-// import CrochetNodeHook from './crochetNodeHook.js'
-// import CrochetNodeLoop from './crochetNodeLoop.js'
-// import CrochetNodeStruct from './crochetNodeStruct.js'
-// import CrochetNodeCHSpaceStart from './crochetNodeCHSpaceStart.js'
-// import CrochetNodeCHSpaceCont from './crochetNodeCHSpaceCont.js'
 
 const crochetNodeFactory = new FactoryClass(CrochetNode, false, 0)
 crochetNodeFactory
   .registerClass(CrochetNode, 'default', ['default', false, 'black'])
-  // .registerClass(CrochetNodeOrigin)
   .registerClass(CrochetNodeFixed, 'origin', ['origin', true, 'black'])
-  // .registerClass(CrochetNodeStart)
   .registerClass(CrochetNode, 'start', ['start', false, 'green'])
-  // .registerClass(CrochetNodeFinish)
   .registerClass(CrochetNode, 'finish', ['finish', true, 'green'])
-  // .registerClass(CrochetNodeHook)
   .registerClass(CrochetNode, 'hook', ['hook', false, 'brown'])
-  // .registerClass(CrochetNodeLoop)
   .registerClass(CrochetNode, 'loop', ['loop', true, 'blue'])
-  // .registerClass(CrochetNodeStruct)
   .registerClass(CrochetNode, 'struct', ['struct', true, 'darkgray'])
-  // .registerClass(CrochetNodeCHSpaceStart)
   .registerClass(CrochetNode, 'ch_sp_start', ['ch_sp_start', true, 'yellow'])
-  // .registerClass(CrochetNodeCHSpaceCont)
   .registerClass(CrochetNode, 'ch_sp_cont', ['ch_sp_cont', true, 'violet'])
 
 export default crochetNodeFactory
--- src/engine/stitches/stitchFactory.js
+++ src/engine/stitches/stitchFactory.js
@@ -1,25 +1,14 @@
 import FactoryClass from '../misc/factory.js'
+
 import CrochetStitch from './crochetStitch.js'
-
-// import CrochetStitchOrigin from './crochetStitchOrigin.js'
-// import CrochetStitchChain from './crochetStitchChain.js'
-// import CrochetStitchSingle from './crochetStitchSingle.js'
-// import CrochetStitchDouble from './crochetStitchDouble.js'
-// import CrochetStitchSlip from './crochetStitchSlip.js'
-
 const CrochetStitchFactory = new FactoryClass(CrochetStitch, false, 4)
 
 CrochetStitchFactory
   .registerClass(CrochetStitch, 'default', ['default', true, 0, 'mk:default:default'])
-  // .registerClass(CrochetStitchOrigin)
   .registerClass(CrochetStitch, 'origin', ['origin', false, 0, 'makeorigin'])
-  // .registerClass(CrochetStitchChain)
   .registerClass(CrochetStitch, 'ch', ['ch', true, 0, 'mk:external:start; mk:draw:finish:5:ch'])
-  // .registerClass(CrochetStitchSingle)
   .registerClass(CrochetStitch, 'sc', ['sc', true, 1, 'mk:external:start; mk:sequence:finish; mv:other; mk:external:hook; mk:draw:loop:5:sc; merge:right'])
-  // .registerClass(CrochetStitchDouble)
   .registerClass(CrochetStitch, 'dc', ['dc', true, 1, 'mk:external:start; mk:sequence:finish; mv:other; mk:external:hook; mk:draw:loop:15:dc; merge:right'])
-  // .registerClass(CrochetStitchSlip)
   .registerClass(CrochetStitch, 'slst', ['slst', true, 1, 'mk:external:start; mk:sequence:finish; mv:other; mk:external:hook; merge:right'])
 
 export default CrochetStitchFactory

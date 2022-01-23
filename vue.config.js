e6e09b0fd1a3cda64082b6f78cf52f3a7c614830
Jacek Galkowski <38131406+trekski@users.noreply.github.com>
2022-01-23 12:14:16 +0100

    V0.9.1 factory overhaul 2021.01.10 (#4)
    * refactored Factory class, simplified Nodes factory
    
    * refactored Stitch factory
    
    * deleted files which are no longer necessary
    
    * rename CrochetNodeOrigin to ...Fixed
    
    * remove unnecessary comments
    
    * create new class - for drawable lines dictionary
    
    * remove all console.log()
    
    * remove more console.log()
    
    * fixed all refactoring.  what stays: when cal. neighborts in CLAC DEF NEW POS remove "from" node fro mneightbors
    
    * remove all of console.log()
    
    * exclude toNode for CoM calc in CALC_DEF_NEW_POS
    
    * fine tune the physics to get better layout
    
    * tinker wit the PWA manifest and such
    
    * some more fun with refreshing service workers etc.
    
    * finish the worker experimetns for now

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,31 @@ module.exports = {
     demo: {
       entry: 'src/demo.js',
       title: 'Demo'
+    },
+  },
+  pwa: {
+    name: 'Doilify',
+    themeColor: 'DarkGoldenRod',
+    msTileColor: 'DarkGoldenRod',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'DarkGoldenRod',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    // workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      // swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    },
+    manifestOptions: {
+      name: 'Doilify',
+      short_name: 'Doilify',
+      description: 'An app to help you design your crochet doilies!',
+      // icons: [
+      //   {"src":"./img/icons/myicon.png","sizes":"512x512","type":"image/png"}
+      // ],
+      background_color: 'DarkGoldenRod',
     }
   }
 }

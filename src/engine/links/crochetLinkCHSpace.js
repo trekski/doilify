4e53217ca033caeb5a7a93a93435dece1d403ebf
Jacek Galkowski <38131406+trekski@users.noreply.github.com>
2022-01-28 11:00:59 +0100

    remove redundant link classes

--- src/engine/links/crochetLinkCHSpace.js
+++
@@ -1,9 +0,0 @@
-import CrochetLink from './crochetLink.js'
-
-class CrochetLinkCHSpace extends CrochetLink {
-  get type () { return 'chain_space' }
-  get defLen () { return 10 }
-  get desc () { return 'links in a chainspace' }
-}
-
-export default CrochetLinkCHSpace
--- src/engine/links/crochetLinkZero.js
+++
@@ -1,9 +0,0 @@
-import CrochetLink from './crochetLink.js'
-
-class CrochetLinkZero extends CrochetLink {
-  get type () { return 'zero' }
-  get defLen () { return 0 }
-  get desc () { return 'zero-lenght link' }
-}
-
-export default CrochetLinkZero

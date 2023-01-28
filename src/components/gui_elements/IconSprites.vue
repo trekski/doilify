5f3ec3b343b01226af28f8d379f9eabafa9c00ad
Jacek Galkowski <38131406+trekski@users.noreply.github.com>
2023-01-28 18:48:14 +0100

    tool to parse Inkscape Icosn to vue component

---
+++ src/components/gui_elements/IconSprites.vue
@@ -0,0 +1,50 @@
+<template>
+  <svg
+    xmlns:svg="http://www.w3.org/2000/svg"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <symbol
+      id="svg-icon-test1"
+      viewBox="0 0 40 40"
+    >
+      <path
+        id="path844"
+        class="svg-icon-main"
+        d="m 11.16,24.7 c -0.25,-6.67 2.01,-14.81 9.04,-13.04 6.2,1.5 10.89,5.02 8.56,11.67 z"
+      />
+      <path
+        id="path846"
+        class="svg-icon-accent"
+        d="m 20.85,32.59 3.34,-5.45 8.28,5.26 z"
+      />
+    </symbol>
+    <symbol
+      id="svg-icon-test2"
+      viewBox="41 0 40 40"
+    >
+      <path
+        id="path893"
+        class="svg-icon-main"
+        d="m 51.44,10.79 2.22,10.79 3.13,-6.45 7.46,-1.11 -3.93,-4.34 0.61,-5.04 -5.95,-0.5 -6.76,-2.42 0.71,4.54 -3.03,5.24 z"
+      />
+      <rect
+        id="xcv"
+        class="svg-icon-main"
+        x="1"
+        y="1"
+        width="2"
+        height="3"
+      />
+      <path
+        id="path895"
+        class="svg-icon-accent"
+        d="m 59.61,34.19 c 0.3,-4.34 -8.88,-3.43 0.0,-4.44 8.88,-1.01 2.32,-6.56 7.46,-3.03 5.14,3.53 2.72,4.03 2.22,6.15 -0.5,2.12 -7.67,-0.4 -9.68,1.31 z"
+      />
+    </symbol>
+  </svg>
+</template>
+
+<style>
+  .svg-icon-main {fill: var(--color); stroke: var(--color)}
+  .svg-icon-accent {fill: var(--color2); stroke: var(--color2)}
+</style>
--- src/pages/Svg.vue
+++ src/pages/Svg.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <SvgIcons style="display: none"/>
-    <svg style="border:solid 1px red; width:50px; height:50px" >
-      <use class="icon" xlink:href="#symbol1865"/>
+    <SvgIcons style="display: none" />
+    <svg
+      style="border:solid 1px red; width:50px; height:50px"
+      class="icon"
+    >
+      <use xlink:href="#svg-icon-test1" />
+    </svg>
+    <svg
+      style="border:solid 1px red; width:50px; height:50px"
+      class="icon2"
+    >
+      <use xlink:href="#svg-icon-test1" />
+    </svg>
+    <svg
+      style="border:solid 1px red; width:50px; height:50px"
+      class="icon2"
+    >
+      <use xlink:href="#svg-icon-test2" />
     </svg>
   </div>
 </template>
 
 <script>
-import SvgIcons from '../components/svgIcons.vue'
+// import SvgIcons from '../components/svgIcons.vue'
+import SvgIcons from '../components/gui_elements/IconSprites.vue'
 
 export default {
-  name: 'svg',
+  name: 'Svg',
   components: {
     SvgIcons: SvgIcons
   }
@@ -19,6 +35,8 @@ export default {
 </script>
 
 <style>
-.icon {fill: red; color:blue; border: solid 1px red}
-.icon:hover {fill: yellow; color:green}
+  .icon {--color: magenta; --color2: green;}
+  .icon:hover {--color: pink; --color2: lightgreen;}
+  .icon2 {--color: blue; --color2: gold;}
+  .icon2:hover {--color: cyan; --color2: yellow;}
 </style>
